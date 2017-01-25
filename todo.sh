619a8f88846549a5c6c94b69bb0cc214f3bdd474
Thibault <thibault@monsieurtshirt.com>
2017-01-26 00:51:22 +0100

    add todo save

--- todo.sh
+++ todo.sh
@@ -2,6 +2,13 @@ todoFunc(){
     if [[ $1 == "see" ]]
     then
 	    cat -n "${TODO_PATH}/$2"
+    elif [[ $1 == "save" ]]
+    then
+        cd $TODO_PATH 
+	git add .
+	git commit -m "todo updated" 
+	git push origin master 
+	cd $OLDPWD
     elif [[ $1 == "add" ]]
     then
         echo "$3" >> ${TODO_PATH}/$2
