4ed429c261ebbe19d1afd94dcbbf2cb8481b6245
Thibault <thibault@monsieurtshirt.com>
2017-02-23 16:12:28 +0100

    install open to user input

--- install.sh
+++ install.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 os=$("uname")
+if [[ $os == "Darwin" ]]
+then
+    BASH_AUTOCOMPLETE_ADVICE=/usr/local/etc/bash_completion.d/todo
+    else
+    BASH_AUTOCOMPLETE_ADVICE=/etc/bash_completion.d/todo
+fi
+
+echo "to set value as default, just press enter"
+read -p $'Choose the path for your todo directory : \x0a default is ~/todo ' TODO_PATH
+TODO_PATH=${TODO_PATH:=~/todo}
+read -p $'Choose the path for your ssh public key : \x0a default is ~/.ssh/id_rsa ' SSH_PATH
+SSH_PATH=${SSH_PATH:=~/.ssh/id_rsa}
+read -p $'Choose the path for your bash autoComplete file : \x0a default is \x0a
+you seem to be on '"$os"$', \x0a so adviced one is '"$BASH_AUTOCOMPLETE_ADVICE"' ' BASH_AUTOCOMPLETE_PATH
+BASH_AUTOCOMPLETE_PATH=${BASH_AUTOCOMPLETE_PATH:=$BASH_AUTOCOMPLETE_ADVICE}
+
+echo "#here begin the lines added by todo_list_github" >> ~/.zshrc
+echo "export TODO_PATH=${TODO_PATH}" >> ~/.zshrc
+echo "export SSH_PATH=${SSH_PATH}" >> ~/.zshrc
+echo "export BASH_AUTOCOMPLETE_PATH=${BASH_AUTOCOMPLETE_PATH}" >> ~/.zshrc
 
 cat zshrc >> ~/.zshrc
 cp githubPulling.sh ~
 cp todo.sh ~
-mkdir -p ~/todo
+mkdir -p "$TODO_PATH"
 while true; do
     read -p "Do you wish to install Github auto-pulling [y/n]?" yn
     case $yn in
@@ -21,11 +41,7 @@ fi
 if [ "$os" == "Darwin" ]
 then
 	compinstall
-	echo "Installation is almost complete !
-as you are on MAC OSX, run the following to finish installing :
-cp todo /usr/local/etc/bash_completion.d/"
-else
-	echo "Installation is almost complete !
-as you are on LINUX, run the following to finish installing :
-sudo cp todo /etc/bash_completion.d/"
 fi
+echo "Installation is almost complete ! Please run the following to finish
+sudo cp todo ${BASH_AUTOCOMPLETE_PATH}"
+echo "#here end the lines added by todo_list_github" >> ~/.zshrc
--- zshrc
+++ zshrc
@@ -1,29 +1,19 @@
-
-#here begin the lines added by todo_list_github
 os=$("uname")
-
-#it's up to you to set those variables
-export TODO_PATH=~/todo
-export SSH_PATH=~/.ssh/id_rsa
 
 #export SSH_KEY=your_ssh_passphrase (optional)
 
 #maybe you would have to change those var also
 if [[ $os == "Darwin" ]]
 then
-    export BASH_AUTOCOMPLETE_PATH=/usr/local/etc/bash_completion.d/todo
     zstyle :compinstall filename '~/.zshrc'
 
     autoload compinit
     compinit
 #have to remove those file to keep [vim TAB] working nicely
     rm ~/.zcompdum*
-else
-    export BASH_AUTOCOMPLETE_PATH=/etc/bash_completion.d/todo
 fi
 
 autoload bashcompinit
 bashcompinit
 source ${BASH_AUTOCOMPLETE_PATH}
 source ~/todo.sh
-#here end the lines added by todo_list_github
